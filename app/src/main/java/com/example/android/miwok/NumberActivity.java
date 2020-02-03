8a9673c130f93f7cd8b77188b17b8324bc813ea9
WesRocha <39282367+WesRocha@users.noreply.github.com>
2020-02-03 16:58:06 -0300

    my third commit

--- app/src/main/java/com/example/android/miwok/NumberActivity.java
+++ app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -2,6 +2,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.LogPrinter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumberActivity extends AppCompatActivity {
 
@@ -10,19 +16,25 @@ public class NumberActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
 
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        ArrayList<String> words = new ArrayList<>();     //        String[] words = new String[10];
 
+        words.add("one");        //        words[0] = "one";
+        words.add("two");        //        words[1] = "two";
+        words.add("three");      //        words[2] = "three";
+        words.add("four");       //        words[3] = "four";
+        words.add("five");       //        words[4] = "five";
+        words.add("six");        //        words[5] = "six";
+        words.add("seven");      //        words[6] = "seven";
+        words.add("eight");      //        words[7] = "eight";
+        words.add("nine");       //        words[8] = "nine";
+        words.add("ten");        //        words[9] = "ten";
 
-
+        LinearLayout numbersFather = (LinearLayout) findViewById(R.id.numbersFather);
+        int i;
+        for (i = 0; i < words.size(); i++){
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            numbersFather.addView(wordView);
+        }
     }
 }
